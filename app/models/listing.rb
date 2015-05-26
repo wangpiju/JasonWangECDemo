60456bfc0964fb1308c2580f710a2112b5974cbe
Jason Wang <wangpiju0420@gmail.com>
2015-05-26 14:52:25 +0800

    add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,5 +10,10 @@ class Listing < ActiveRecord::Base
 	end
     validates_attachment_content_type :image,
     :content_type => %w(image/jpeg image/jpg image/png)
+
+    validates_attachment_presence :image
+
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: {greater_than: 0}
     
 end
