834d778b50557a081a2393a86fcf32523d1d006a
Jason Wang <wangpiju0420@gmail.com>
2015-05-26 22:48:28 +0800

    Add listings belong user

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -15,5 +15,7 @@ class Listing < ActiveRecord::Base
 
     validates :name, :description, :price, presence: true
     validates :price, numericality: {greater_than: 0}
+
+    belongs_to :user
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524143603) do
+ActiveRecord::Schema.define(version: 20150526143109) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150524143603) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
